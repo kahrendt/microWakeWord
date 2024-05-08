ac1fa9e651903087cebc76e46bc300eb84b56182
Kevin Ahrendt <kahrendt@gmail.com>
2024-05-08 11:59:57 -0400

    add strided keep layer

--- microwakeword/layers/strided_drop.py
+++ microwakeword/layers/strided_drop.py
@@ -34,6 +34,7 @@ class StridedDrop(tf.keras.layers.Layer):
         super(StridedDrop, self).__init__(**kwargs)
         self.time_slices_to_drop = time_slices_to_drop
         self.mode = mode
+        self.state_shape = []
 
     def call(self, inputs):
         if self.mode == modes.Modes.NON_STREAM_INFERENCE:
@@ -48,3 +49,45 @@ class StridedDrop(tf.keras.layers.Layer):
         }
         base_config = super(StridedDrop, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+    def get_input_state(self):
+        return []
+
+    def get_output_state(self):
+        return []    
+class StridedKeep(tf.keras.layers.Layer):
+    """StridedDrop
+
+    Drops the specified audio feature slices in nonstreaming mode only.
+    Used for matching the dimensions of convolutions with valid padding.
+
+    Attributes:
+        time_sclices_to_drop: number of audio feature slices to drop
+        mode: inference mode; e.g., non-streaming, internal streaming
+    """
+
+    def __init__(
+        self, time_slices_to_keep, mode=modes.Modes.NON_STREAM_INFERENCE, **kwargs
+    ):
+        super(StridedKeep, self).__init__(**kwargs)
+        self.time_slices_to_keep = max(time_slices_to_keep,1)
+        self.mode = mode
+        self.state_shape = []
+
+    def call(self, inputs):
+        if self.mode != modes.Modes.NON_STREAM_INFERENCE:
+            return inputs[:, -self.time_slices_to_keep :, :, :]
+
+        return inputs
+
+    def get_config(self):
+        config = {
+            "time_slices_to_keep": self.time_slices_to_keep,
+            "mode": self.mode,
+        }
+        base_config = super(StridedKeep, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+    def get_input_state(self):
+        return []
+
+    def get_output_state(self):
+        return []    
